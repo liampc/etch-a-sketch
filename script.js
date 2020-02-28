b229ec979378591e9bddce448932401842dab035
Kathlia Carlos <lia.carlos23@gmail.com>
2020-02-28 20:22:47 +0800

    add hover effect on box

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
 let divCont = document.querySelector("#container");
 
+
 function addDiv(){
     n = 256; // change to prompt, let number multiply by itself
    
     for (let i = 0; i < n; i++){
         let box = document.createElement('div');
         box.classList.add('divBox');
-        divCont.appendChild(box) ; 
-        
+        divCont.appendChild(box); 
+        box.addEventListener('mouseenter', event => {
+            box.style.background = "red";
+        })
+         
     }
     
 }
-
 

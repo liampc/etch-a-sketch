a2fbbdb38f51194bbdc836c0b11ffc25f251ad5c
Kathlia Carlos <lia.carlos23@gmail.com>
2020-02-28 15:52:19 +0800

    change flex to grid in css

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ let divCont = document.querySelector("#container");
 function addDiv(){
     n = 256;
    //change it in the future based on dimensions of screen
-    for (let i = 0; i < n; i++){
+    first:for (let i = 0; i < n; i++){
         let box = document.createElement('div');
-        box.classList.add('divBox');
-        divCont.appendChild(box);
+            for (let j = 0; j < 16; j++)
+                box.classList.add('divBox');
+                divCont.appendChild(box) ; 
+                continue first;
+        
     }
     
 }
+
 

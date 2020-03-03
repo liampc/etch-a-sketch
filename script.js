54b2d455582f21a44ea4fa5e58e9a70ba142f101
Kathlia Carlos <lia.carlos23@gmail.com>
2020-03-03 09:57:49 +0800

    add button in html

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 
 let divCont = document.querySelector("#container");
+document.body.addEventListener('onload', addDiv(16));
 
-function addDiv(){
-    n = 256; // change to prompt, let number multiply by itself
+function addDiv(side){
+     let n = side * side// change to prompt, let number multiply by itself
    
     for (let i = 0; i < n; i++){
         let box = document.createElement('div');
         box.classList.add('divBox');
-        divCont.appendChild(box) ; 
-        
+        divCont.appendChild(box); 
+        box.addEventListener('mouseenter', event => {
+            box.style.background = "red";
+        })
+         
     }
     
 }
 
-
+let frame;
+let btn = document.querySelector('button');
+btn.addEventListenerss('click', event => {
+    frame = prompt("How many squares per side do you want?");
+    console.log(addDiv(frame));
+})

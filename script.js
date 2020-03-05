3e03ec822349fb78b699147bbb328593ae38b352
Kathlia Carlos <lia.carlos23@gmail.com>
2020-03-05 09:15:46 +0800

    add working grid

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ let divCont = document.querySelector("#container");
 document.body.addEventListener('onload', addDiv(16));
 
 function addDiv(side){
-     let n = side * side// change to prompt, let number multiply by itself
+    divCont.style.gridTemplateRows = `repeat(${side}, 1fr)`;
+    divCont.style.gridTemplateColumns = `repeat(${side}, 1fr)`;
+    let n = side * side// change to prompt, let number multiply by itself
    
     for (let i = 0; i < n; i++){
         let box = document.createElement('div');
         box.classList.add('divBox');
+        box.style.g
         divCont.appendChild(box); 
-        box.addEventListener('mouseenter', event => {
+    
+    box.addEventListener('mouseenter', event => {
             box.style.background = "red";
         })
          
@@ -17,9 +21,3 @@ function addDiv(side){
     
 }
 
-let frame;
-let btn = document.querySelector('button');
-btn.addEventListenerss('click', event => {
-    frame = prompt("How many squares per side do you want?");
-    console.log(addDiv(frame));
-})
